afe6ed1d50ec74635f8ad745b88795c1fb4300bb
zzc <774908356@qq.com>
2017-11-17 15:55:30 +0800

    FirstBuild

--- ZCTodayNews/ViewController.swift
+++ ZCTodayNews/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sss")
+        print("ssssss")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
